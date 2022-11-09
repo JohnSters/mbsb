a7f40d195f50cf49b2605fad6fc220c8570cb8c4
jonathan <jonnyschoeman@gmail.com>
2022-11-09 17:11:30 +0200

    Fixed small issues.

--- pages/index.vue
+++ pages/index.vue
@@ -6,52 +6,97 @@
     <v-row>
       <v-col cols="12" sm="4">
         <v-card class="pa-3">
-          <v-img src="/products/cube.jpeg" contain></v-img>
+          <v-img src="/products/cube.jpeg" width="300" class="ma-auto"></v-img>
           <v-card-title>Product Title: Icecube</v-card-title>
-          <v-card-text>Iodine/ Calcium in Icecube shapes</v-card-text>
+          <v-card-text>(Iodine/Calcium) in Icecube Shapes</v-card-text>
           <p class="orange--text text-center">Price: R 4.20 each</p>
-          <v-btn class="blue darken-3 rounded-0 mt-1" depressed dark block><v-icon class="mr-1">mdi-package</v-icon>Order Now</v-btn>
-          <v-btn class="blue darken-3 rounded-0 mt-1" depressed dark block><v-icon class="mr-1">mdi-whatsapp</v-icon>Whatsapp Order</v-btn>
+          <v-btn
+            href="https://form.jotform.com/223123073382548"
+            target="_blank"
+            class="blue darken-3 rounded-0 mt-1"
+            depressed
+            dark
+            block
+          >
+            <v-icon class="mr-1">mdi-package</v-icon>
+            Order Now
+          </v-btn>
         </v-card>
       </v-col>
       <v-col cols="12" sm="4">
         <v-card class="pa-3">
-          <v-img src="/products/medium.jpeg"></v-img>
-          <v-card-title>Product Title: Medium blocks</v-card-title>
-          <v-card-text>Iodine/ Calcium in Ice cube shapes</v-card-text>
+          <v-img src="/products/medium.jpeg" width="335" class="ma-auto"></v-img>
+          <v-card-title>Product Title: Medium Blocks</v-card-title>
+          <v-card-text>(Iodine/Calcium) in 5x5 blocks</v-card-text>
           <p class="orange--text text-center">Price: R 13.00 each</p>
-          <v-btn class="blue darken-3 rounded-0 mt-1" depressed dark block><v-icon class="mr-1">mdi-package</v-icon>Order Now</v-btn>
-          <v-btn class="blue darken-3 rounded-0 mt-1" depressed dark block><v-icon class="mr-1">mdi-whatsapp</v-icon>Whatsapp Order</v-btn>
+          <v-btn
+            href="https://form.jotform.com/223123073382548"
+            target="_blank"
+            class="blue darken-3 rounded-0 mt-1"
+            depressed
+            dark
+            block
+          >
+            <v-icon class="mr-1">mdi-package</v-icon>
+            Order Now
+          </v-btn>
         </v-card>
       </v-col>
       <v-col cols="12" sm="4">
         <v-card class="pa-3">
-          <v-img src="/products/flight.jpeg"></v-img>
+          <v-img src="/products/flight.jpeg" width="274" class="ma-auto"></v-img>
           <v-card-title>Product Title: Flight blocks </v-card-title>
-          <v-card-text>Iodine/ Calcium in Icecube shapes</v-card-text>
+          <v-card-text>(Iodine/Calcium) Parrot/Flight Block</v-card-text>
           <p class="orange--text text-center">Price: R 36.00 each</p>
-          <v-btn class="blue darken-3 rounded-0 mt-1" depressed dark block><v-icon class="mr-1">mdi-package</v-icon>Order Now</v-btn>
-          <v-btn class="blue darken-3 rounded-0 mt-1" depressed dark block><v-icon class="mr-1">mdi-whatsapp</v-icon>Whatsapp Order</v-btn>
+          <v-btn
+            href="https://form.jotform.com/223123073382548"
+            target="_blank"
+            class="blue darken-3 rounded-0 mt-1"
+            depressed
+            dark
+            block
+          >
+            <v-icon class="mr-1">mdi-package</v-icon>
+            Order Now
+          </v-btn>
         </v-card>
       </v-col>
       <v-col cols="12" sm="4">
         <v-card class="pa-3">
-          <v-img src="/products/powder.jpeg"></v-img>
+          <v-img src="/products/powder.jpeg" width="274" class="ma-auto"></v-img>
           <v-card-title>Product Title: Powder </v-card-title>
-          <v-card-text>Iodine/ Calcium 250g Powder</v-card-text>
+          <v-card-text>(Iodine/Calcium) 250g Powder Bags</v-card-text>
           <p class="orange--text text-center">Price: R 60.00 each</p>
-          <v-btn class="blue darken-3 rounded-0 mt-1" depressed dark block><v-icon class="mr-1">mdi-package</v-icon>Order Now</v-btn>
-          <v-btn class="blue darken-3 rounded-0 mt-1" depressed dark block><v-icon class="mr-1">mdi-whatsapp</v-icon>Whatsapp Order</v-btn>
+          <v-btn
+            href="https://form.jotform.com/223123073382548"
+            target="_blank"
+            class="blue darken-3 rounded-0 mt-1"
+            depressed
+            dark
+            block
+          >
+            <v-icon class="mr-1">mdi-package</v-icon>
+            Order Now
+          </v-btn>
         </v-card>
       </v-col>
       <v-col cols="12" sm="4">
         <v-card class="pa-3">
-          <v-img src="/products/heart.jpeg"></v-img>
+          <v-img src="/products/heart.jpeg" width="212" class="ma-auto"></v-img>
           <v-card-title>Product Title: Heart Shape</v-card-title>
-          <v-card-text>Iodine/ Calcium in Heart Shaped Blocks</v-card-text>
+          <v-card-text>(Iodine/Calcium) in Heart Shaped Blocks</v-card-text>
           <p class="orange--text text-center">Price: R 10.00 each</p>
-          <v-btn class="blue darken-3 rounded-0 mt-1" depressed dark block><v-icon class="mr-1">mdi-package</v-icon>Order Now</v-btn>
-          <v-btn class="blue darken-3 rounded-0 mt-1" depressed dark block><v-icon class="mr-1">mdi-whatsapp</v-icon>Whatsapp Order</v-btn>
+          <v-btn
+          href="https://form.jotform.com/223123073382548"
+          target="_blank"
+          class="blue darken-3 rounded-0 mt-1"
+          depressed
+          dark
+          block
+        >
+          <v-icon class="mr-1">mdi-package</v-icon>
+          Order Now
+        </v-btn>
         </v-card>
       </v-col>
     </v-row>
